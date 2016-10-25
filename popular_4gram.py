4fe5fb32679cc14ffaf2e3ebe943549de6fb38d3
Clayton Condie <clayton.condie@gmail.com>
2016-10-25 00:08:16 -0600

    figured out aws sequence file format. adjust for 4gram, or 1gram test, and get sparkContext to configure.

--- popular_4gram.py
+++ popular_4gram.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     outputFile = sys.argv[3]
 
     # start SparkContext
-    sc = SparkContext("local", "popular_4gram")
+    sc = SparkContext("local[*]", "popular_4gram")
 
     log4jLogger = sc._jvm.org.apache.log4j
     LOGGER = log4jLogger.LogManager.getLogger(__name__)
@@ -22,26 +22,31 @@ if __name__ == '__main__':
     # read input
     LOGGER.info('***** READING LZO HADOOP FILE')
     # LZO indexed by row i.e. <1:ngram data, 2: ngram data, 3: ngram data>
-    files = sc.newAPIHadoopFile(inputPath, "com.hadoop.mapreduce.LzoTextInputFormat", "org.apache.hadoop.io.LongWritable", "org.apache.hadoop.io.Text")
+    files = sc.sequenceFile(inputPath, "org.apache.hadoop.io.LongWritable", "org.apache.hadoop.io.Text")
     lzoRDD = files.map(lambda x: x[1])
 
     # # map to 3-tuples of (ngram, year, count)
     LOGGER.info('***** SPLITTING LZO INPUT')
     allEntries = lzoRDD.map(lambda x: x.split())
-    # allEntries = lines.map(lambda x: x.split())
-    #
+
     LOGGER.info('***** GENERATING 3-TUPLES')
-    formattedEntries = allEntries.map(lambda x: (str(x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3]), x[4], x[5]))
+    # formattedEntries = allEntries.map(lambda x: (str(x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3]), x[4], x[5]))
+    formattedEntries = allEntries.map(lambda x: x[4])
 
     LOGGER.info('***** FILTERING ENTRIES TO 1910')
-    filteredEntries = formattedEntries.filter(lambda x: x[1] == year_to_search)
-
-    LOGGER.info('***** SORT BY OCCURRENCES')
-    sortedEntries = filteredEntries.sortBy(lambda a: a[2])
+    # filteredEntries = formattedEntries.filter(lambda x: x[1] == year_to_search)
+    filteredEntries = formattedEntries.filter(lambda x: x == year_to_search)
 
     LOGGER.info('***** COLLECT AND WRITE TO FILE')
-    sortedEntries.collect()
-    sortedEntries.saveAsTextFile(outputFile)
+    filteredEntries.collect()
+    filteredEntries.saveAsTextFile(outputFile)
+
+    # LOGGER.info('***** SORT BY OCCURRENCES')
+    # sortedEntries = filteredEntries.sortBy(lambda a: a[2])
+
+    # LOGGER.info('***** COLLECT AND WRITE TO FILE')
+    # sortedEntries.collect()
+    # sortedEntries.saveAsTextFile(outputFile)
 
     sc.stop()
     sys.exit(0)
