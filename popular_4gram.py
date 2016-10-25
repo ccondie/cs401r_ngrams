1a225b9530151286559aee051ce493c483da34f7
Clayton Condie <clayton.condie@gmail.com>
2016-10-25 09:30:45 -0600

    altered how the spark context is created. now runs to completion on aws emr

--- popular_4gram.py
+++ popular_4gram.py
@@ -1,6 +1,7 @@
 import sys
 from operator import add
 from pyspark import SparkContext
+from pyspark import SparkConf
 import logging
 import re
 import os
@@ -13,7 +14,8 @@ if __name__ == '__main__':
     outputFile = sys.argv[3]
 
     # start SparkContext
-    sc = SparkContext("local[*]", "popular_4gram")
+    conf = SparkConf().setAppName('popular_4gram')
+    sc = SparkContext(conf=conf)
 
     log4jLogger = sc._jvm.org.apache.log4j
     LOGGER = log4jLogger.LogManager.getLogger(__name__)
@@ -30,23 +32,22 @@ if __name__ == '__main__':
     allEntries = lzoRDD.map(lambda x: x.split())
 
     LOGGER.info('***** GENERATING 3-TUPLES')
-    # formattedEntries = allEntries.map(lambda x: (str(x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3]), x[4], x[5]))
-    formattedEntries = allEntries.map(lambda x: x[4])
+    # 1gram: x[0]:word - x[1]:year - x[2]:count - x[3]:pages - x[4]:books
+    # 4gram: x[0]:word1 - x[1]:word2 - x[2]:word3 - x[3]:word4 - x[4]:year - x[5]:count - x[6]:pages - x[7]:books
+    formattedEntries = allEntries.map(lambda x: (str(x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3]), x[4], x[5]))
+    # formattedEntries - "word word word word", 1905, 54
+    # formattedEntries = allEntries.map(lambda x: x[1])
 
-    LOGGER.info('***** FILTERING ENTRIES TO 1910')
-    # filteredEntries = formattedEntries.filter(lambda x: x[1] == year_to_search)
-    filteredEntries = formattedEntries.filter(lambda x: x == year_to_search)
+    LOGGER.info('***** FILTERING ENTRIES TO INPUTTED YEAR')
+    filteredEntries = formattedEntries.filter(lambda x: x[1] == year_to_search)
+    # filteredEntries = formattedEntries.filter(lambda x: x == year_to_search)
+
+    LOGGER.info('***** SORT BY OCCURRENCES')
+    sortedEntries = filteredEntries.sortBy(lambda a: a[2])
 
     LOGGER.info('***** COLLECT AND WRITE TO FILE')
-    filteredEntries.collect()
-    filteredEntries.saveAsTextFile(outputFile)
-
-    # LOGGER.info('***** SORT BY OCCURRENCES')
-    # sortedEntries = filteredEntries.sortBy(lambda a: a[2])
-
-    # LOGGER.info('***** COLLECT AND WRITE TO FILE')
-    # sortedEntries.collect()
-    # sortedEntries.saveAsTextFile(outputFile)
+    sortedEntries.collect()
+    sortedEntries.saveAsTextFile(outputFile)
 
     sc.stop()
     sys.exit(0)
